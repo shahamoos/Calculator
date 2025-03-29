25b419e2908660233fe90e1e3b53c2062611e371
shahamoos <46780864+shahamoos@users.noreply.github.com>
2025-03-29 21:21:21 +0530

    Update calculator.rb
    Check presence of delimiter

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,9 @@
 def calculator(str)
-  numbers = str.split(/[\n,]/)
+  if str.start_with?("//")
+    delimiter = str[2] 
+    numbers = numbers.split(delimiter)
+  end
+  #numbers = str.split(/[\n,]/)
   sum = 0
   numbers.each do |n|
     sum+=n.to_i
