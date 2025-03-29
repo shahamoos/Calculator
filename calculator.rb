9ec124e4b7a4e9ecca6b8f5826a97e95d59fa03d
shahamoos <46780864+shahamoos@users.noreply.github.com>
2025-03-29 21:27:20 +0530

    Update calculator.rb
    Handle string in case of with or without explicitly defined delimiter

--- calculator.rb
+++ calculator.rb
@@ -2,11 +2,13 @@ def calculator(str)
   if str.start_with?("//")
     delimiter = str[2] 
     numbers = str.split(delimiter)
+    numbers.shift
+  else
+    numbers = str.split(/[\n,]/)
   end
-  #numbers = str.split(/[\n,]/)
   sum = 0
-  numbers.each do |n|
-    sum+=n.to_i
-  end
+  #numbers.each do |n|
+    #sum+=n.to_i
+  #end
   sum
 end
